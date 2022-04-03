780ef22d6659561d442b9912a4f34c009ab3b8b1
Pavitra <pareshraut50@gmail.com>
2022-04-03 20:04:41 +0530

    Initial commit

---
+++ app/src/main/java/com/rex50/kawaspaceassignment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rex50.kawaspaceassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
